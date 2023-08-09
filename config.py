c191ffca2bb4c2e68100addd973a9bd5567c7852
clovis <clovistumasang82@gmail.com>
2023-08-09 16:15:24 +0100

    feat: dev_update

--- config.py
+++ config.py
@@ -1,3 +1,19 @@
 from flask import Flask 
-
+import os
 app = Flask(__name__)
+
+
+
+
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(file))
+
+# Enable debug mode.
+DEBUG = True
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+# Connect to the database
+# TODO IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:clovertt123@localhost:5432/dbname'
