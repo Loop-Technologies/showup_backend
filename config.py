42fac519572b39ae087f331e5d1801133e2b1e76
clovis739 <clovistumasang82@gmail.com>
2023-08-19 17:48:21 +0100

    modified

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+from flask import Flask,session
 import os
+from flask_session import Session
 
+
+
+app = Flask(__name__)
+
+
+Session(app)
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -12,5 +20,14 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # Connect to the database
 # TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:teming334@localhost/showup'
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 
 
