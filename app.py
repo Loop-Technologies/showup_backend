9c0a0944b0923f3f9e718f9874a8a93b0bf23d69
mrnguchi <munohnguchi334@gmail.com>
2023-08-25 18:42:18 +0100

    feat: registration integrated with landing page and accepted registration interfaces

--- app.py
+++ app.py
@@ -12,19 +12,6 @@ from config import app
 
 
 
-
-# app = Flask(__name__)
-
-# from venue_listing import venue_listing as venue_listing
-# app.register_blueprint(venue_listing)
-
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-# app.config['SECRET_KEY'] = SECRET_KEY
-
-
-
-
 login_manager = LoginManager()
 # login_manager.login_view = 'app.login'
 login_manager.init_app(app)
@@ -42,12 +29,9 @@ session = sessionmaker(bind=engine)
 
 
 
-
-
-
 @app.route('/')
 def home():
-    return render_template("./pages/home.html")
+    return render_template("./pages/landing.html")
 
 
 
@@ -74,23 +58,20 @@ def fan_signup():
             #checking to see if the email entered has already been used
             if new_fan is not None:
                 flash('That email has been used! Try another email','error')
-               
+                return render_template("./forms/fan_signup.html",fullname = fullname, phonenumber = phonenumber, user_email = user_email, password = password)
             else:
                 try:
                     new_user = Fan(full_name=fullname,email=user_email, phone_number=phonenumber,password=hashed_password)
                     db.session.add(new_user)
                     db.session.commit()
                     
-                    flash('Account created!', 'success')
                     return redirect(url_for('fan_login'))
                     
                 except:
                     flash('failed to register!', 'error')
-                    return render_template('./forms/fan_registration.html')
+                    return render_template('./forms/fan_signup.html')
              
-        else:
-            flash('Passwords do not match','error')
-    return render_template("./forms/fan_registration.html")
+    return render_template("./forms/fan_signup.html")
             
 
 
@@ -111,43 +92,24 @@ def fan_login():
             is_valid = check_password_hash(user_password, password)
             if is_valid:
                 # login_user(user)
-                return render_template('./pages/home.html')
+                # return render_template('./pages/home.html')
+                 return render_template("./pages/landing.html")
+                
             else:
                 flash('Incorrect password','error')
+                return render_template("./forms/login.html", user_email = user_email )
         else:
             
             flash('Account doesnt exit','error')
-            return render_template('./forms/fan_login.html',email = user_email)
-    return render_template("./forms/fan_login.html")
+            # return render_template('./forms/login.html')
+            redirect(url_for('fan_login'))
+    return render_template("./forms/login.html")
             
 
   
 
 
-#admin views for the various tables in the database
-admin = Admin(app)  
-admin.add_view(ModelView(Fan, db.session))
-admin.add_view(ModelView(Artist, db.session))
-admin.add_view(ModelView(SysAdmin, db.session))
-  
 
-
-
-
-#tesing database connection         
-@app.route('/get_data')
-def get_fan_data():
-    data = Fan.query.all()
-    
-    return render_template('./forms/test.html', data=data)
-
-
-#tesing database connection         
-@app.route('/get_artist_data')
-def get_artist_data():
-    data = Artist.query.all()
-    
-    return render_template('./forms/test1.html', data=data)
 
 
 
@@ -161,40 +123,40 @@ def artist_signup():
         password =  request.form.get('password')
         password1 = request.form.get('password1') #password confirmation
     
-        if password == password1:
         
-
-            #eliminating spaces in password string             
-            # password_str = ""
-            # for char in password:
-            #     if char != "":
-            #         password_str += char
-            #creating a password hash
-            hashed_password = generate_password_hash(password)
+        
+        hashed_password = generate_password_hash(password)
        
-            #create a new user and save his/her data to the database
-            new_artist = Artist.query.filter_by(username=u_name).first()
-            
-            #checking to see if the email entered has already been used
-            if new_artist is not None:
-                flash('That username has been used! Try another email','error')
-               
-            else:
-                try:
-                    new_user = Artist(username=u_name,email=user_email, phone_number=phonenumber,password=hashed_password)
-                    db.session.add(new_user)
-                    db.session.commit()
-                    #login_user(new_user, remember=True)
-                    flash('Account created!', 'success')
-                    # return redirect(url_for('artist_login'))
-                    
-                except:
-                    flash('failed to register!', 'error')
-                    return render_template('./forms/artist_registration.html')
-             
+        #create a new user and save his/her data to the database
+        new_artist = Artist.query.filter_by(username=u_name).first()
+        artist_email = Artist.query.filter_by(email = user_email).first()
+        #checking to see if the email entered has already been used
+        if new_artist is not None:
+            flash('That username has been used! Try another usernanme','error')
+            return render_template("./forms/artist_signup.html", username = u_name, phonenumber= phonenumber, email = user_email, password = password, password1 = password1 )
+
+        elif artist_email is not None: 
+            flash('That email has been used! Try another email','error')
+            return render_template("./forms/artist_signup.html", username = u_name, phonenumber= phonenumber, email = user_email, password = password, password1 = password1 )   
+
         else:
-            flash('Passwords do not match','error')
-    return render_template("./forms/artist_registration.html")
+            # try:
+                new_user = Artist(username=u_name,email=user_email, phone_number=phonenumber,password=hashed_password)
+                db.session.add(new_user)
+                db.session.commit()
+                #login_user(new_user, remember=True)
+                    
+                # return redirect(url_for('artist_login'))
+                flash('Account created')
+                    
+            # except:
+                
+            #     # return render_template('./forms/artist_signup.html')
+            #     flash('Account not created')
+            #     return render_template("./forms/artist_signup.html", username = u_name, phonenumber= phonenumber, email = user_email, password = password, password1 = password1 )   
+                
+        
+    return render_template("./forms/artist_signup.html")
             
 
 
@@ -212,55 +174,17 @@ def artist_login():
             user_password = user.password
             is_valid = check_password_hash(user_password, password)
             if is_valid:
-                return render_template('./pages/home.html')
+                return render_template('./pages/landing.html')
             else:
-                flash('Invalid password')
+                flash('Incorrect password')
+                return render_template('./forms/login.html', email = user_email, password = password)
+                
         else:
             
             flash('Account doesnt exist','error')
-            return render_template('./forms/login.html')
-    return render_template("./forms/artist_login.html")
+            return render_template('./forms/login.html', email = user_email, password = password)
+    return render_template("./forms/login.html")
             
-
-
-
-
-
-
-
-
-# #pagination of venues 
-# @app.route("/venues", methods = ["GET", "POST"])
-# def venues():
- 
-#     venues = fan.query.paginate(page=1,per_page = 4)
-#     print(venues.items)
-#     print(venues.pages)
-#     Paginated_items= []
-#     #  for i in range (venues.pages):
-        
-#     return render_template("pages/venues.html",venues = venues)    
-            
-# #loading more items to the page
-# @app.route("/venues/load_more",methods = ["GET", "POST"])
-# def load_more():
-#     venues = fan.query.paginate(page=2,per_page = 4)
-#     if request.method == "post":
-        
-#         print(venues.items)           
-#         # return render_template("pages/venues.html",venues = venues)
-
-
-#     return render_template("pages/venues.html",venues = venues)
-
-
-
-@app.route("/venues/<int:page_num>")
-def venues_listing(page_num):
-    venues = Fan.query.paginate(page=page_num, per_page = 6, error_out = False)
-    return render_template("pages/venues.html",venues = venues)
-
-
 
 if __name__ == '__main__':  
   app.run(debug=True)
